1ff971bb57e0d5b43216953e39bd5fee383315c4
Olcay Ertaşmış <ertasolcay@hotmail.com>
2023-10-06 20:00:02 +0300

    SlideColumn
    columns can be scrolled up and down

--- Assets/_GameFolder/Scripts/GridSystem/GridMovement.cs
+++ Assets/_GameFolder/Scripts/GridSystem/GridMovement.cs
@@ -1,19 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 
 namespace _GameFolder.Scripts.GridSystem
 {
-    public enum Direction
-    {
-        None,
-        Up,
-        Down,
-        Left,
-        Right
-    }
-
-
     public class GridMovement : MonoBehaviour
     {
         private Vector2 _firstTouchPos, _finalTouchPos;
@@ -29,6 +20,9 @@ namespace _GameFolder.Scripts.GridSystem
 
         private RaycastHit2D _firstHitInformation;
 
+        private bool _canInteract = true;
+        private bool _canInteract2;
+
         private void Awake()
         {
             _cam = Camera.main;
@@ -38,26 +32,29 @@ namespace _GameFolder.Scripts.GridSystem
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0)) OnInteraction();
-            if (Input.GetMouseButtonUp(0)) NotInteraction();
+            if (Input.GetMouseButtonDown(0) && _canInteract) OnInteraction();
+            if (Input.GetMouseButtonUp(0) && _canInteract2) NotInteraction();
         }
 
         private void OnInteraction()
         {
+            _canInteract = false;
+            _canInteract2 = true;
+
             _firstTouchPos = _cam.ScreenToWorldPoint(Input.mousePosition);
             _firstHitInformation = Physics2D.Raycast(_firstTouchPos, _cam.transform.forward);
-            Debug.Log("first " + _firstTouchPos);
+            //Debug.Log("first " + _firstTouchPos);
         }
 
         private void NotInteraction()
         {
+            _canInteract2 = false;
+            _canInteract = true;
+
             if (_firstHitInformation.collider == null) return;
 
             _finalTouchPos = _cam.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log("final " + _finalTouchPos);
-
-            /*RaycastHit2D finalHitInformation = Physics2D.Raycast(_finalTouchPos, _cam.transform.forward);
-            if (finalHitInformation.collider == null) return;*/
+            //Debug.Log("final " + _finalTouchPos);
 
             CalculateAngle();
         }
@@ -83,11 +80,11 @@ namespace _GameFolder.Scripts.GridSystem
                     break;
                 case > 45 and <= 135: // yukarı
                     Debug.Log("YUKARI");
-                    SwipeColumn(1);
+                    SlideColumn(1);
                     break;
                 case < -45 and >= -135: // aşağı
                     Debug.Log("AŞAĞI");
-                    SwipeColumn(-1);
+                    SlideColumn(-1);
                     break;
                 case > 135 or < -135: // sola
                     Debug.Log("SOLA");
@@ -95,17 +92,88 @@ namespace _GameFolder.Scripts.GridSystem
             }
         }
 
-        private void SwipeColumn(int addOrSubtract)
+        private void SlideColumn(int addOrSubtract)
         {
             var fruitPos = _firstHitInformation.transform.position;
             var columnOfFruit = _gridSpawner.FruitColumns[(int)fruitPos.x];
 
-            foreach (var fruit in columnOfFruit)
+            GameObject newFruit = null;
+
+
+            switch (addOrSubtract)
             {
-                var pos = fruit.transform.position;
-                pos.y += addOrSubtract;
-                fruit.transform.DOMoveY(pos.y, .5f);
+                case > 0:
+                    foreach (var fruit in columnOfFruit)
+                    {
+                        var pos = fruit.transform.position;
+                        pos.y += addOrSubtract;
+
+                        fruit.transform.DOMoveY(pos.y, 0.5f).OnComplete(delegate
+                        {
+                            if (fruit == columnOfFruit[^1])
+                            {
+                                CloneOverflowFruit(columnOfFruit, columnOfFruit.Count - 1);
+
+                                fruit.gameObject.SetActive(false);
+                            }
+                        });
+                    }
+
+                    break;
+                case < 0:
+                    for (int i = columnOfFruit.Count - 1; i >= 0; i--)
+                    {
+                        var pos = columnOfFruit[i].transform.position;
+                        pos.y += addOrSubtract;
+                        columnOfFruit[i].transform.DOMoveY(pos.y, 0.5f);
+
+                        if (columnOfFruit[i] == columnOfFruit[0])
+                        {
+                            newFruit = CloneOverflowFruit(columnOfFruit, 0);
+
+                            columnOfFruit[i].gameObject.SetActive(false);
+                        }
+                    }
+
+                    break;
             }
+
+            if (newFruit != null)
+            {
+                columnOfFruit.RemoveAt(0);
+                columnOfFruit.Add(newFruit);
+            }
+        }
+
+
+        private GameObject CloneOverflowFruit(List<GameObject> columnList, int index)
+        {
+            var columnInFruitCount = columnList.Count;
+            var fruit = columnList[index];
+            var pos = fruit.transform.position;
+
+            switch (index)
+            {
+                case > 0:
+                    columnList.RemoveAt(index);
+                    pos.y = -1;
+                    var spawnPos = new Vector3(pos.x, pos.y, pos.z);
+                    var newFruit = Instantiate(fruit, spawnPos, Quaternion.identity);
+                    newFruit.name = "Fruit : " + pos.x + ", " + pos.y;
+                    columnList.Insert(0, newFruit);
+                    newFruit.transform.DOMoveY(0, .1f);
+                    break;
+                case 0:
+                    pos.y = columnInFruitCount;
+                    var spawnPos2 = new Vector3(pos.x, pos.y, pos.z);
+                    var newFruit2 = Instantiate(fruit, spawnPos2, Quaternion.identity);
+                    newFruit2.name = "Fruit : " + pos.x + ", " + pos.y;
+
+                    newFruit2.transform.DOMoveY(columnInFruitCount - 1, .1f);
+                    return newFruit2;
+            }
+
+            return null;
         }
     }
 }
