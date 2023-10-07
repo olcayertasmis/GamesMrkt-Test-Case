e816d3eb4ce78d155202cb1413c6ddbc5676dba8
Olcay Ertaşmış <ertasolcay@hotmail.com>
2023-10-07 20:54:52 +0300

    Cell in fruit update

--- Assets/_GameFolder/Scripts/GridSystem/Cell.cs
+++ Assets/_GameFolder/Scripts/GridSystem/Cell.cs
@@ -1,16 +1,31 @@
+using System;
 using UnityEngine;
 
 namespace _GameFolder.Scripts.GridSystem
 {
     public class Cell : MonoBehaviour
     {
+        private Vector2 _cellPos;
+
         public bool IsEmpty { get; private set; }
         public Fruit FruitInCell { get; private set; }
 
-        public void SetFruits(Fruit fruit)
+        public Action<Vector2, Fruit> OnChangedFruit;
+
+        public void Initialize(Fruit fruit, Vector2 pos)
+        {
+            _cellPos = pos;
+
+            FruitInCell = fruit;
+            IsEmpty = false;
+        }
+
+        public void UpdateFruits(Fruit fruit)
         {
             FruitInCell = fruit;
             IsEmpty = false;
+
+            OnChangedFruit?.Invoke(_cellPos, fruit);
         }
 
         public void ClearCell()
