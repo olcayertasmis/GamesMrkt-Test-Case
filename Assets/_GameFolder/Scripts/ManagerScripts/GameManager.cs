335d7c6c94199e385d9b157d4d27c2e8be58bf7a
Olcay Ertaşmış <ertasolcay@hotmail.com>
2023-10-08 04:02:08 +0300

    Update GameManager.cs

--- Assets/_GameFolder/Scripts/ManagerScripts/GameManager.cs
+++ Assets/_GameFolder/Scripts/ManagerScripts/GameManager.cs
@@ -1,8 +1,28 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace _GameFolder.Scripts.ManagerScripts
 {
     public class GameManager : MonoBehaviour
     {
+        [SerializeField] private GameObject nextLevelPanel;
+
+        private void Awake()
+        {
+            nextLevelPanel.SetActive(false);
+        }
+
+        public void NextLevel()
+        {
+            Managers.Instance.DataManager.AllLevels.activeLevel++;
+
+            /*activeLevelIndex++;
+
+            Managers.Instance.LevelManager.SetActiveLevel(activeLevelIndex);*/
+
+            var scene = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(scene);
+        }
     }
 }
