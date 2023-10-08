a458c976100ca5a4dc560d347bcb3dfb332b12fa
Olcay Ertaşmış <ertasolcay@hotmail.com>
2023-10-08 04:02:50 +0300

    --

--- Assets/_GameFolder/Scripts/ManagerScripts/LevelManager.cs
+++ Assets/_GameFolder/Scripts/ManagerScripts/LevelManager.cs
@@ -1,29 +1,18 @@
+using System;
 using UnityEngine;
 
 namespace _GameFolder.Scripts.ManagerScripts
 {
     public class LevelManager : MonoBehaviour
     {
-        private int _activeLevel = 0;
-
-        private void Start()
-        {
-            InitializeLevel();
-        }
-
-        public int GetActiveLevel()
+        /*public int GetActiveLevel()
         {
             return _activeLevel;
-        }
+        }*/
 
         public void SetActiveLevel(int level)
         {
-            _activeLevel = level;
-        }
-
-
-        private void InitializeLevel()
-        {
+            Managers.Instance.DataManager.AllLevels.activeLevel = level;
         }
     }
 }
