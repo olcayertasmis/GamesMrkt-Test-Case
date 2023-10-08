620f372b49a2ea6a0d6b56ed53748f395c13ee35
Olcay Ertaşmış <ertasolcay@hotmail.com>
2023-10-08 20:43:26 +0300

    Moved different folder

---
+++ Assets/_GameFolder/Scripts/GridSystem/Fruit.cs
@@ -0,0 +1,25 @@
+using _GameFolder.Scripts.Enums;
+using UnityEngine;
+
+namespace _GameFolder.Scripts.GridSystem
+{
+    public class Fruit : MonoBehaviour
+    {
+        [SerializeField] private FruitColor fruitColor;
+
+        #region Getters
+
+        public FruitColor FruitColorType => fruitColor;
+
+        #endregion
+
+        public static Fruit FruitSpawn(Fruit prefab, Vector2 pos, Transform parent)
+        {
+            var fruit = Instantiate(prefab, pos, Quaternion.identity);
+            fruit.transform.parent = parent;
+            fruit.name = "Fruit : " + pos.x + ", " + pos.y;
+
+            return fruit;
+        }
+    } // END CLASS
+}
