2545d880ef04129123177c869997d9f450c510df
Olcay Ertaşmış <ertasolcay@hotmail.com>
2023-10-08 20:42:10 +0300

    Deleted

--- Assets/_GameFolder/Scripts/ManagerScripts/LevelManager.cs
+++ Assets/_GameFolder/Scripts/ManagerScripts/LevelManager.cs
@@ -1,18 +1,8 @@
-using System;
 using UnityEngine;
 
 namespace _GameFolder.Scripts.ManagerScripts
 {
     public class LevelManager : MonoBehaviour
     {
-        /*public int GetActiveLevel()
-        {
-            return _activeLevel;
-        }*/
-
-        public void SetActiveLevel(int level)
-        {
-            Managers.Instance.DataManager.AllLevels.activeLevel = level;
-        }
     }
 }
