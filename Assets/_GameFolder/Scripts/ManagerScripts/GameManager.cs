d6063473a07a58095a7945e69b453c20a1e43a13
Olcay Ertaşmış <ertasolcay@hotmail.com>
2023-10-08 05:38:43 +0300

    Updated GameManager.cs

--- Assets/_GameFolder/Scripts/ManagerScripts/GameManager.cs
+++ Assets/_GameFolder/Scripts/ManagerScripts/GameManager.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using _GameFolder.Scripts.Enums;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,18 +11,69 @@ namespace _GameFolder.Scripts.ManagerScripts
     {
         [SerializeField] private GameObject nextLevelPanel;
 
-        private void Awake()
+        public List<FruitColor> requiredColor;
+        public List<int> requiredValue;
+
+        public List<int> activeTrueMatchCount;
+        private List<bool> _trueMatchControl;
+
+        private void Start()
         {
             nextLevelPanel.SetActive(false);
+
+            var level = Managers.Instance.DataManager.AllLevels.LevelList[Managers.Instance.DataManager.AllLevels.activeLevel];
+            var req = level.requiredValues;
+            foreach (var x in req)
+            {
+                requiredColor.Add(x.requiredColor[0]);
+                requiredValue.Add(x.requiredColorNumber[0]);
+                activeTrueMatchCount.Add(0);
+                _trueMatchControl.Add(false);
+            }
+        }
+
+        private void Update()
+        {
+            OpenNextLevelPanel();
+        }
+
+        private void OpenNextLevelPanel()
+        {
+            bool allTrue = AreAllTrue(_trueMatchControl);
+
+            if (allTrue) nextLevelPanel.SetActive(true);
+        }
+
+        private void Control()
+        {
+            for (int i = 0; i < requiredColor.Count; i++)
+            {
+                if (requiredValue[i] == activeTrueMatchCount[i])
+                {
+                    _trueMatchControl[i] = true;
+                }
+            }
+        }
+
+        private bool AreAllTrue(List<bool> boolList)
+        {
+            return boolList.All(item => item); // Tüm değerler "true" ise true döner, aksi halde false döner
+        }
+
+        public void MatchValueControl(FruitColor fruitColor)
+        {
+            for (int i = 0; i < requiredColor.Count; i++)
+            {
+                if (fruitColor == requiredColor[i] && requiredValue[i] >= activeTrueMatchCount[i])
+                {
+                    activeTrueMatchCount[i]++;
+                }
+            }
         }
 
         public void NextLevel()
         {
             Managers.Instance.DataManager.AllLevels.activeLevel++;
-
-            /*activeLevelIndex++;
-
-            Managers.Instance.LevelManager.SetActiveLevel(activeLevelIndex);*/
 
             var scene = SceneManager.GetActiveScene().name;
             SceneManager.LoadScene(scene);
